b349d249bd773c7c83f08e3c0d0ecfb1bcb27c22
GraniLuk <granica.lukasz@gmail.com>
2025-11-30 17:31:03 +0100

    Refactor HTML structure and improve code formatting in index.html and preview.js

--- static/admin/preview.js
+++ static/admin/preview.js
@@ -1,15 +1,15 @@
 CMS.registerPreviewTemplate('recipes', ({ entry }) => {
-  const title = entry.getIn(['data', 'title']);
-  const body = entry.getIn(['data', 'body']);
-  const tagline = entry.getIn(['data', 'tagline']);
-  const author = entry.getIn(['data', 'author']);
+    const title = entry.getIn(['data', 'title']);
+    const body = entry.getIn(['data', 'body']);
+    const tagline = entry.getIn(['data', 'tagline']);
+    const author = entry.getIn(['data', 'author']);
 
-  return (
-    <div>
-      <h1>{title}</h1>
-      {tagline && <p>{tagline}</p>}
-      {author && <p>Autor: {author}</p>}
-      <div dangerouslySetInnerHTML={{ __html: marked(body || '') }} />
-    </div>
-  );
+    return (
+        <div>
+            <h1>{title}</h1>
+            {tagline && <p>{tagline}</p>}
+            {author && <p>Autor: {author}</p>}
+            <div dangerouslySetInnerHTML={{ __html: marked(body || '') }} />
+        </div>
+    );
 });
